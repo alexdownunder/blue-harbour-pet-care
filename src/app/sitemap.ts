c83aa04f69033f644e1ef994808a802b723700dd
alexdownunder <synthaizex@gmail.com>
2026-03-13 16:06:53 +1100

    Add sitemap route

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,12 +11,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/thank-you`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.3,
-    },
-    {
       url: `${baseUrl}/terms`,
       lastModified: new Date(),
       changeFrequency: "yearly",
